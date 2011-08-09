29b7a42c52536af74821af649598a28bb66f60c6
Bo Shi <bo@brooklynpacket.com>
2011-08-09 15:26:04 -0700

    Arg.

--- fabutil2.py
+++ fabutil2.py
@@ -181,7 +181,8 @@ def setup_user_runit(acct=None, home=None):
                    'exec chpst -u{acct} svlogd -tt {runit_log_dir}/\n')
     
     sudo('mkdir -p {runit_log_dir}')
-    sudo('chown -R {acct}:{acct} {home}/{{service,shared}}')
+    sudo('mkdir -p {home}/{{shared,service}}')
+    sudo('chown -R {acct}:{acct} {home}')
         
     sudo('mkdir -p /etc/service/{acct}/log')
     sudo('mkdir -p /etc/sv/{acct}')
